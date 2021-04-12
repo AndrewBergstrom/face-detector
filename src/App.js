a67b63ceb2447bbd83d14c230563018ed8bd36d5
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-04-12 14:54:43 -0700

    input and submit button functionality added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,50 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 import ImageLinkForm from "./components/ImageLinkForm";
+import Rank from "./components/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 350,
+      },
+    },
+  },
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onButtonSubmit}
+        />
+
         {/* { <FaceRecognition />} */}
       </div>
     );
