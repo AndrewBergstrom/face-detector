cee285e319e17edcb331f307f590ebe32bb8b35c
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-04-12 11:48:35 -0700

    url input form added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import Navigation from './components/Navigation';
-import Logo from './components/Logo';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation";
+import Logo from "./components/Logo";
+import ImageLinkForm from "./components/ImageLinkForm";
+import "./App.css";
 
-class  App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-      <Logo />
-        {/* {  <ImageLinkForm />
-        <FaceRecognition />} */}
+        <Logo />
+        <ImageLinkForm />
+        {/* { <FaceRecognition />} */}
       </div>
     );
   }
