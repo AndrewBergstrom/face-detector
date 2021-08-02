6c43851040576de6b48ae5ed768b647aae420d1b
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-08-02 11:20:55 -1000

    updating onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -80,10 +80,15 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) =>
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then((response) => {
+        if(response){
+          fetch('http://localhost:3000/image')
+        }
         this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+  })
       .catch((err) => console.log(err));
   };
 
